5e3911715ec008ed1fbcb6af44661fde5151a606
juanmagv <jmgonvaz@gmail.com>
2021-07-16 16:23:43 +0200

    Insertado enunciado del ejercicio

--- ejexrcicio consola banco/ejexrcicio consola banco/Program.cs
+++ ejexrcicio consola banco/ejexrcicio consola banco/Program.cs
@@ -7,9 +7,14 @@ using System.Threading.Tasks;
 namespace ejexrcicio_consola_banco
 {
     class Program
-    {
+        
+    { //ejercicio para gestionar las cuentas de un banco
+       //se pide crear una clase de tipo cuenta para gestionar ingresos y gastos
+        
         static void Main(string[] args)
         {
+            
+
         }
     }
 }
