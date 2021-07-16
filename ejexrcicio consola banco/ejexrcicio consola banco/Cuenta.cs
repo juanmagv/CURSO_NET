c37556b548c3d5d8e8bfd2439aa0c429ec59e9a3
juanmagv <jmgonvaz@gmail.com>
2021-07-16 17:42:50 +0200

    Creado la clase  y el constructor falta menu

---
+++ ejexrcicio consola banco/ejexrcicio consola banco/Cuenta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejexrcicio_consola_banco
+{
+    class Cuenta
+    { 
+        private int numCuenta;
+        private string numCliente;
+        private double saldo;
+
+        public Cuenta()
+        {
+
+        }
+        public int PnumCuenta
+        {
+            set { numCuenta = value; }
+            get { return numCuenta; }
+        }
+        public string PnumCliente
+        {
+            set { numCliente = value; }
+            get { return numCliente; }
+        }
+        public double Psaldo
+        {
+            set { saldo = value; }
+            get { return saldo; }
+        }
+        public void consultaDatos()
+        {
+            Console.WriteLine("Numero de cuenta: " + this.PnumCuenta + "\n Numero de cliente: " + this.PnumCliente + "\n saldo: " + this.Psaldo);
+        }
+
+        public void depositar(int deposito)
+        {
+            this.saldo = this.saldo + deposito;
+        }
+        public void retirar(int retiro)
+        {
+            this.saldo = this.saldo -retiro;
+        }
+
+
+    }
+}
--- ejexrcicio consola banco/ejexrcicio consola banco/Program.cs
+++ ejexrcicio consola banco/ejexrcicio consola banco/Program.cs
@@ -13,9 +13,42 @@ namespace ejexrcicio_consola_banco
         
         static void Main(string[] args)
         {
+            int deposito = 0;
+            int retiro = 0;
+            Cuenta cuen = new Cuenta();
 
+            string opcion = "";
             
+            do
+            {
+                Console.WriteLine("Crear cuenta");
+                Console.WriteLine("Depositar cantidad");
+                Console.WriteLine("Retirar dinero");
+                Console.WriteLine("Consultar saldo");
+                Console.WriteLine("Salir de la App");
+                Console.WriteLine("introduzaca una opcion");
+                opcion = Console.ReadLine();
+                Console.Clear();
 
+                switch (opcion)
+                {
+                    case "1":
+
+
+                        Console.WriteLine("Introduce el numero de cuenta");
+                        cuen.PnumCuenta = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Introduce el numero de cliente");
+                        cuen.PnumCliente = Console.ReadLine();
+                        Console.WriteLine("introduzca el saldo");
+                        cuen.Psaldo = double.Parse(Console.ReadLine());
+                        break;
+
+
+
+                    case "2":
+                }
+
+            } while (opcion != "5");
         }
     }
 }
