1349ed048f0d73dcc8f64b1c15b0a71390953a71
juanmagv <jmgonvaz@gmail.com>
2021-07-30 20:11:59 +0200

    finalizado diseño

---
+++ altas bajas y modificacione LINQ tp SQL 2/altas bajas y modificacione LINQ tp SQL 2/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace altas_bajas_y_modificacione_LINQ_tp_SQL_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEdad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbCasado = new System.Windows.Forms.CheckBox();
+            this.btAlta = new System.Windows.Forms.Button();
+            this.btBaja = new System.Windows.Forms.Button();
+            this.btMod = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Location = new System.Drawing.Point(106, 13);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btBuscar.TabIndex = 0;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(65, 73);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(65, 116);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(65, 155);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Apellido";
+            // 
+            // txtEdad
+            // 
+            this.txtEdad.Location = new System.Drawing.Point(65, 194);
+            this.txtEdad.Name = "txtEdad";
+            this.txtEdad.Size = new System.Drawing.Size(100, 20);
+            this.txtEdad.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Edad";
+            // 
+            // cbCasado
+            // 
+            this.cbCasado.AutoSize = true;
+            this.cbCasado.Location = new System.Drawing.Point(28, 244);
+            this.cbCasado.Name = "cbCasado";
+            this.cbCasado.Size = new System.Drawing.Size(62, 17);
+            this.cbCasado.TabIndex = 10;
+            this.cbCasado.Text = "Casado";
+            this.cbCasado.UseVisualStyleBackColor = true;
+            // 
+            // btAlta
+            // 
+            this.btAlta.Location = new System.Drawing.Point(28, 294);
+            this.btAlta.Name = "btAlta";
+            this.btAlta.Size = new System.Drawing.Size(75, 23);
+            this.btAlta.TabIndex = 11;
+            this.btAlta.Text = "Alta";
+            this.btAlta.UseVisualStyleBackColor = true;
+            // 
+            // btBaja
+            // 
+            this.btBaja.Location = new System.Drawing.Point(28, 347);
+            this.btBaja.Name = "btBaja";
+            this.btBaja.Size = new System.Drawing.Size(75, 23);
+            this.btBaja.TabIndex = 12;
+            this.btBaja.Text = "Baja";
+            this.btBaja.UseVisualStyleBackColor = true;
+            // 
+            // btMod
+            // 
+            this.btMod.Location = new System.Drawing.Point(28, 399);
+            this.btMod.Name = "btMod";
+            this.btMod.Size = new System.Drawing.Size(75, 23);
+            this.btMod.TabIndex = 13;
+            this.btMod.Text = "Modif.";
+            this.btMod.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(354, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Sistema deGestion de Hospital";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(213, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(463, 349);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btMod);
+            this.Controls.Add(this.btBaja);
+            this.Controls.Add(this.btAlta);
+            this.Controls.Add(this.cbCasado);
+            this.Controls.Add(this.txtEdad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btBuscar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEdad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox cbCasado;
+        private System.Windows.Forms.Button btAlta;
+        private System.Windows.Forms.Button btBaja;
+        private System.Windows.Forms.Button btMod;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ altas bajas y modificacione LINQ tp SQL 2/altas bajas y modificacione LINQ tp SQL 2/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace altas_bajas_y_modificacione_LINQ_tp_SQL_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        } DataClasses1DataContext dbhospital = new DataClasses1DataContext();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void CargarGrid()
+        {
+            var cargarGrid = from u in dbhospital.usuarios
+            select u.id;
+            dataGridView1.DataSource = cargarGrid;
+
+
+        }
+    }
+}
