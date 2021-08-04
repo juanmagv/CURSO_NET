4f9a9f3749b75e1333885412cdb06a2bfa10b358
juanmagv <jmgonvaz@gmail.com>
2021-08-04 17:40:44 +0200

    CAsi finalizado

---
+++ Dia 3 del 8 CON LINQ/Dia 3 del 8 CON LINQ/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace Dia_3_del_8_CON_LINQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInscripcion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rbHombre = new System.Windows.Forms.RadioButton();
+            this.rbMujer = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSS = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btGuardar = new System.Windows.Forms.Button();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.BtActualizar = new System.Windows.Forms.Button();
+            this.btBorrar = new System.Windows.Forms.Button();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.dpfechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inscripcion";
+            // 
+            // txtInscripcion
+            // 
+            this.txtInscripcion.Location = new System.Drawing.Point(110, 29);
+            this.txtInscripcion.Name = "txtInscripcion";
+            this.txtInscripcion.Size = new System.Drawing.Size(185, 20);
+            this.txtInscripcion.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(110, 71);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(185, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre y Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(68, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sexo";
+            // 
+            // rbHombre
+            // 
+            this.rbHombre.AutoSize = true;
+            this.rbHombre.Location = new System.Drawing.Point(119, 113);
+            this.rbHombre.Name = "rbHombre";
+            this.rbHombre.Size = new System.Drawing.Size(62, 17);
+            this.rbHombre.TabIndex = 5;
+            this.rbHombre.TabStop = true;
+            this.rbHombre.Text = "Hombre";
+            this.rbHombre.UseVisualStyleBackColor = true;
+            // 
+            // rbMujer
+            // 
+            this.rbMujer.AutoSize = true;
+            this.rbMujer.Location = new System.Drawing.Point(198, 115);
+            this.rbMujer.Name = "rbMujer";
+            this.rbMujer.Size = new System.Drawing.Size(51, 17);
+            this.rbMujer.TabIndex = 6;
+            this.rbMujer.TabStop = true;
+            this.rbMujer.Text = "Mujer";
+            this.rbMujer.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Fecha";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 196);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Direccion";
+            // 
+            // txtSS
+            // 
+            this.txtSS.Location = new System.Drawing.Point(103, 244);
+            this.txtSS.Name = "txtSS";
+            this.txtSS.Size = new System.Drawing.Size(180, 20);
+            this.txtSS.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 247);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Numero de la SS";
+            // 
+            // btGuardar
+            // 
+            this.btGuardar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btGuardar.Location = new System.Drawing.Point(1, 283);
+            this.btGuardar.Name = "btGuardar";
+            this.btGuardar.Size = new System.Drawing.Size(58, 23);
+            this.btGuardar.TabIndex = 13;
+            this.btGuardar.Text = "Guardar";
+            this.btGuardar.UseVisualStyleBackColor = true;
+            this.btGuardar.Click += new System.EventHandler(this.btGuardar_Click);
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btBuscar.Location = new System.Drawing.Point(65, 283);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(59, 23);
+            this.btBuscar.TabIndex = 14;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            this.btBuscar.Click += new System.EventHandler(this.btBuscar_Click);
+            // 
+            // BtActualizar
+            // 
+            this.BtActualizar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtActualizar.Location = new System.Drawing.Point(130, 283);
+            this.BtActualizar.Name = "BtActualizar";
+            this.BtActualizar.Size = new System.Drawing.Size(75, 23);
+            this.BtActualizar.TabIndex = 15;
+            this.BtActualizar.Text = "Actualizar";
+            this.BtActualizar.UseVisualStyleBackColor = true;
+            this.BtActualizar.Click += new System.EventHandler(this.BtActualizar_Click);
+            // 
+            // btBorrar
+            // 
+            this.btBorrar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btBorrar.Location = new System.Drawing.Point(211, 283);
+            this.btBorrar.Name = "btBorrar";
+            this.btBorrar.Size = new System.Drawing.Size(51, 23);
+            this.btBorrar.TabIndex = 16;
+            this.btBorrar.Text = "Borrar";
+            this.btBorrar.UseVisualStyleBackColor = true;
+            this.btBorrar.Click += new System.EventHandler(this.btBorrar_Click);
+            // 
+            // btSalir
+            // 
+            this.btSalir.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btSalir.Location = new System.Drawing.Point(268, 283);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(44, 23);
+            this.btSalir.TabIndex = 17;
+            this.btSalir.Text = "Salir";
+            this.btSalir.UseVisualStyleBackColor = true;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(321, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(851, 273);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(105, 196);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(190, 20);
+            this.txtDireccion.TabIndex = 10;
+            // 
+            // dpfechaNacimiento
+            // 
+            this.dpfechaNacimiento.Location = new System.Drawing.Point(105, 149);
+            this.dpfechaNacimiento.Name = "dpfechaNacimiento";
+            this.dpfechaNacimiento.Size = new System.Drawing.Size(190, 20);
+            this.dpfechaNacimiento.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(321, 324);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 20;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(124, 324);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(1218, 362);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dpfechaNacimiento);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btSalir);
+            this.Controls.Add(this.btBorrar);
+            this.Controls.Add(this.BtActualizar);
+            this.Controls.Add(this.btBuscar);
+            this.Controls.Add(this.btGuardar);
+            this.Controls.Add(this.txtSS);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rbMujer);
+            this.Controls.Add(this.rbHombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInscripcion);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Grabar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInscripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rbHombre;
+        private System.Windows.Forms.RadioButton rbMujer;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSS;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btGuardar;
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.Button BtActualizar;
+        private System.Windows.Forms.Button btBorrar;
+        private System.Windows.Forms.Button btSalir;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.DateTimePicker dpfechaNacimiento;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Dia 3 del 8 CON LINQ/Dia 3 del 8 CON LINQ/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dia_3_del_8_CON_LINQ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataClasses1DataContext Hospital1 = new DataClasses1DataContext();
+        DateTime date = DateTime.Now;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var cargaGrid = from h in Hospital1.Enfermo1 select h;
+            dataGridView1.DataSource = cargaGrid;
+        }
+
+
+        void CargaGrid()
+        {
+            var cargarGrid = from h in Hospital1.Enfermo1 select h;
+            dataGridView1.DataSource = cargarGrid;
+        }
+
+        private void btGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Enfermo1 MyEnfermo = new Enfermo1();
+                MyEnfermo.inscripcion = Int32.Parse(txtInscripcion.Text);
+                MyEnfermo.Apellido = txtNombre.Text;
+                if (rbHombre.Checked == true)
+                {
+                    MyEnfermo.S = "H";
+                }
+                else
+                {
+                    if (rbMujer.Checked == true)
+                    {
+                        MyEnfermo.S = "M";
+                    }
+                }
+                MyEnfermo.Fecha_Nac = dpfechaNacimiento.Value;
+                MyEnfermo.Direccion = txtDireccion.Text;
+                MyEnfermo.NSS = Int32.Parse(txtSS.Text);
+                Hospital1.Enfermo1.InsertOnSubmit(MyEnfermo);
+                Hospital1.SubmitChanges();
+                label8.Text = "DATOS GUARDADOS";
+                label8.ForeColor = Color.Black;
+                label8.BackColor = Color.Red;
+                CargaGrid();
+                txtInscripcion.Text = "";
+                txtNombre.Text = "";
+                txtDireccion.Text = "";
+                txtSS.Text = "";
+                rbHombre.Checked = false;
+                rbMujer.Checked = false;
+                dpfechaNacimiento.Value = date;
+              
+              
+
+            }
+            catch
+            {
+                if (txtInscripcion.Text == " " || txtNombre.Text == " " || txtDireccion.Text == " " || txtSS.Text == " " ||
+                    (rbHombre.Checked == false && rbMujer.Checked == false) || dpfechaNacimiento.Value == date)
+                {
+                    MessageBox.Show("FALTAN DATOS");
+                }
+                else
+                {
+                    MessageBox.Show("ERROR");
+                }
+            }
+          
+        }
+
+        private void btBuscar_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void BtActualizar_Click(object sender, EventArgs e)
+        {
+            DateTime fecha = new DateTime();
+           
+            try
+            {
+                Enfermo1 MyEnfermo = Hospital1.Enfermo1.Single(h =>
+          h.inscripcion == Int32.Parse(txtInscripcion.Text));
+                MyEnfermo.inscripcion = Int32.Parse(txtInscripcion.Text);
+                MyEnfermo.Apellido = txtNombre.Text;
+                MyEnfermo.NSS = Int32.Parse(txtSS.Text);
+                MyEnfermo.Fecha_Nac = dpfechaNacimiento.Value;
+                MyEnfermo.Direccion = txtDireccion.Text;
+                if (rbHombre.Checked == true)
+                {
+                    MyEnfermo.S = "H";
+                }
+                else
+                {
+                    if (rbMujer.Checked == true)
+                    {
+                        MyEnfermo.S = "M";
+                    }
+                }
+
+                Hospital1.SubmitChanges();
+                CargaGrid();
+                txtInscripcion.Text = "";
+                txtNombre.Text = "";
+                txtDireccion.Text = "";
+                txtSS.Text = "";
+                rbHombre.Checked = false;
+                rbMujer.Checked = false;
+                dpfechaNacimiento.Value = date;
+                label8.Text = "DATOS MODIFICADOS";
+                label8.ForeColor = Color.Black;
+                label8.BackColor = Color.Red;
+               
+
+            }
+            catch
+            {
+
+                if (txtInscripcion.Text == " " || txtNombre.Text == " " || txtDireccion.Text == " " || txtSS.Text == " " ||
+                    (rbHombre.Checked == false && rbMujer.Checked == false) || dpfechaNacimiento.Value == date)
+                {
+                    MessageBox.Show("FALTAN DATOS");
+                }
+                else
+                {
+                    MessageBox.Show("ERROR");
+                }
+            }
+           
+        }
+
+        private void btBorrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Enfermo1 MyEnfermo = Hospital1.Enfermo1.Single(h =>
+              h.inscripcion == Int32.Parse(txtInscripcion.Text));
+                Hospital1.Enfermo1.DeleteOnSubmit(MyEnfermo);
+                Hospital1.SubmitChanges();
+                CargaGrid();
+                txtInscripcion.Text = "";
+                txtNombre.Text = "";
+                txtDireccion.Text = "";
+                txtSS.Text = "";
+                rbHombre.Checked = false;
+                rbMujer.Checked = false;
+                dpfechaNacimiento.Value = date;
+                label8.Text = "DATOS BORRADOS";
+                label8.ForeColor = Color.Black;
+                label8.BackColor = Color.Red;
+          
+
+            }
+            catch
+            {
+                if (txtInscripcion.Text == "")
+                {
+                    MessageBox.Show("Faltan Datos");
+                }
+            }
+
+
+        }
+
+        private void btSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
