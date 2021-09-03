083995c3ef04f586ac11e40e9a162eab20d16965
juanmagv <jmgonvaz@gmail.com>
2021-09-03 19:43:23 +0200

    d

---
+++ ModeloVistaControladorViernes/ModeloVistaControladorViernes/Controllers/DemoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModeloVistaControladorViernes.Models;
+
+namespace ModeloVistaControladorViernes.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+           
+            var libro = new Libro { Isbn = "1122", Titulo = "El principito", TipoLibro = "Novela" };
+            return View(libro);
+        }
+    }
+}
---
+++ ModeloVistaControladorViernes/ModeloVistaControladorViernes/Models/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+namespace ModeloVistaControladorViernes.Models
+{
+   
+   
+    public class Libro
+
+    {
+        public string Isbn { get; set; }
+        public string Titulo { get; set; }
+        public string TipoLibro { get; set; }
+
+    }
+    }
+
